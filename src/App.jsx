29791787204a2e8f626c729d0c4e5d1dc9a51f23
Muneeb381a <muneebafzal381a@gmail.com>
2024-09-23 13:10:47 +0500

    creating different pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Country from "./pages/Country";
 import Contact from "./pages/Contact";
+import AppLayout from "./components/Layout/AppLayout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-  {
-    path: "country",
-    element: <Country />,
-  },
-  {
-    path: "contact",
-    element: <Contact />,
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />, // Correct JSX syntax
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
